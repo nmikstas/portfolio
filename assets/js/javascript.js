2556dbe1d8436c54120778af0c8c0efd1904bccf
NickM <nmikstas@alumni.scu.edu>
2019-10-27 03:46:29 -0600

    Added fog effects

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -744,6 +744,50 @@ var createScene = function ()
         })
     );
 
+    /************************************ Fog Particle System ************************************/
+    var fountain = BABYLON.Mesh.CreateBox("foutain", .01, scene);
+    fountain.visibility = 0;
+
+    // Create a particle system
+    var particleSystem;
+
+    var fogTexture = new BABYLON.Texture("https://raw.githubusercontent.com/aWeirdo/Babylon.js/master/smoke_15.png", scene);
+
+    var createNewSystem = function() {
+        if (particleSystem) {
+            particleSystem.dispose();
+        }
+
+        particleSystem = new BABYLON.ParticleSystem("particles", 100 , scene);
+        particleSystem.manualEmitCount = particleSystem.getCapacity();
+        particleSystem.minEmitBox = new BABYLON.Vector3(-15, 1, -15); // Starting all from
+        particleSystem.maxEmitBox = new BABYLON.Vector3(15, 1, 15); // To...
+        
+        particleSystem.particleTexture = fogTexture.clone();
+        particleSystem.emitter = fountain;
+        
+	    particleSystem.color1 = new BABYLON.Color4(0.8, 0.8, 0.8, 0.1);
+        particleSystem.color2 = new BABYLON.Color4(.95, .95, .95, 0.15);
+        particleSystem.colorDead = new BABYLON.Color4(0.9, 0.9, 0.9, 0.1);
+	    particleSystem.minSize = 3.5;
+        particleSystem.maxSize = 5.0;
+        particleSystem.minLifeTime = Number.MAX_SAFE_INTEGER;
+        particleSystem.emitRate = 50000;
+        particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_STANDARD;
+        particleSystem.gravity = new BABYLON.Vector3(0, 0, 0);
+        particleSystem.direction1 = new BABYLON.Vector3(0, 0, 0);
+        particleSystem.direction2 = new BABYLON.Vector3(0, 0, 0);
+        particleSystem.minAngularSpeed = -2;
+	    particleSystem.maxAngularSpeed = 2;
+        particleSystem.minEmitPower = .5;
+        particleSystem.maxEmitPower = 1;
+        particleSystem.updateSpeed = 0.005;
+    
+        particleSystem.start();
+    }
+
+    createNewSystem();
+
     /******************************** Advanced Animation Updates *********************************/
     var alpha = 0;
     var isRedLightOn = false;
